5f9939da0dd91c261cbb6ba6b69dd767f6cb9b93
SanthoshVelaga <santhosh@Santhoshs-MacBook-Pro.local>
2019-07-06 11:16:31 -0700

    this is my first commit

---
+++ App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import Form from './Form';
+
+function App() {
+  return (
+    <div className="title">
+      Please enter your data here!
+      <Form />
+    </div>
+  );
+}
+
+export default App;
---
+++ Form.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+class Form extends React.Component {
+    render () {
+        return (
+        <div class="wrapper">
+            <form>
+                <input class="firstinput" type="text">
+                </input>
+            </form>
+        </div>
+        );
+    }
+}
+
+export default Form;
